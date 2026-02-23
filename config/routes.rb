3f4f5f37e3a42310dd63d6019e561989f4b88702
Nando2003 <nandofontes27@gmail.com>
2026-02-23 10:49:50 -0300

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  resource :session, only: %i[new create destroy]
-  get '/login', to: 'sessions#new'
-  resource :registration, only: %i[new create]
-  resources :passwords, param: :token
+  inertia "login" => "Auth/Login"
+  inertia "register" => "Auth/Register"
 
+  resource :session, only: %i[new create destroy]
+  resource :registration, only: %i[create]
+  
+  resources :passwords, param: :token
+  
   # Redirect to localhost from 127.0.0.1 to use same IP address with Vite server
   constraints(host: "127.0.0.1") do
     get "(*path)", to: redirect { |params, req| "#{req.protocol}localhost:#{req.port}/#{params[:path]}" }
