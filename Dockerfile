5047c4ca093ab7fc3a1333ab8e2229cde333ce7e
Nando2003 <nandofontes27@gmail.com>
2026-02-23 00:04:59 -0300

    first commit

---
+++ Dockerfile
@@ -0,0 +1,29 @@
+FROM ruby:3.4-alpine
+
+WORKDIR /app
+
+RUN apk add --no-cache \
+  build-base \
+  postgresql-dev \
+  tzdata \
+  yaml-dev \
+  pkgconf \
+  git \
+  bash \
+  nodejs \
+  npm \
+  python3 \
+  libc6-compat
+
+COPY Gemfile Gemfile.lock ./
+RUN bundle install
+
+COPY package.json package-lock.json ./
+RUN npm ci --include=optional
+
+COPY . .
+
+RUN chmod +x bin/dev
+
+EXPOSE 3000 5173
+CMD ["bin/dev"]
---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite'
+import RubyPlugin from 'vite-plugin-ruby'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    tailwindcss(),
+    RubyPlugin(),
+  ],
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      host: "localhost",
+      port: 5173,
+    },
+  }
+})
